7d8f79a0367218bb8a5034b2ab04ccce206e7e20
Benevant Mathew @build3 <benevantmathewv@gmail.com>
2026-02-23 02:24:54 +0530

    dependency issue, released v0.3.0 to pypi

--- auraview/version.py
+++ auraview/version.py
@@ -4,7 +4,7 @@ version.py
 Author: Benevant Mathew
 Date: 2026-02-23
 """
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __author__ = "Benevant Mathew"
 __email__ = "benevantmathewv@gmail.com"
 __release_date__="23-02-2026"
